ab32ef9a3ec1040d82e527f28933be1405abad3f
Danh Nguyen <danh.nguyen@bd.com>
2025-09-26 16:40:32 -0700

    - fixed the application from throwing error on logout - fixed application by making sure the useMatch is always called even if there are logout user, before useMatch stop getting called and that's when react started screaming

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "../hooks/useAuth";
 
 export function AuthLayout() { //The AuthLayout Component is use inside App Component to setup all routes to include AuthLayout as the top header
     const outlet = useOutlet(); // The userOutlet returns nested Route which will be render inside this compnent
-    const user = useLoaderData(); // 
+    const user = useLoaderData();
 
     return (
         <Suspense fallback={<LinearProgress />}>
@@ -16,7 +16,7 @@ export function AuthLayout() { //The AuthLayout Component is use inside App Comp
             <Alert severity="error">Something went wrong loading user data!</Alert>
         ) : (
             /* Pass the loaded value to the AuthProvider */
-            <AuthProvider user={user}>
+            <AuthProvider userData={user}>
             {/* Render the child route (e.g., HomePage, LoginPage, etc.) */}
                 {outlet}
             </AuthProvider>
--- src/components/ProtectedPageLayout.jsx
+++ src/components/ProtectedPageLayout.jsx
@@ -23,21 +23,23 @@ import { MainAppBar } from './AppBar';
 
 export function ProtectedLayout() {
     const outlet = useOutlet();
-
     const { user, is2FAVerified } = useAuth(); //This is a self made hook utilizing a couple of method that is passed as JS Object properties, the method update the state of user which is passed as a variable from the hook as well
 
+    const isOnLogin = useMatch("/login");
+    const isOnVerify2FA = useMatch("/verify-2fa");
+
     // Case #1: If user is not sign in and current path is not login, lets get them to the login page so they can sign in.
-    if (!user && !useMatch("/login")) {
+    if (!user && !isOnLogin) {
         return <Navigate to="/login" />
     }
 
     // Case #2: If user account is not verified and they are not at the verified page then take them there so they can verify
-    if (!is2FAVerified && !useMatch("/verify-2fa")) {
+    if (!is2FAVerified && !isOnVerify2FA) {
         return <Navigate to="/verify-2fa" />
     }
 
     // Case #3: if the user has login, and already verified and they are trying to login or verify again then take them to profile because they shouudn't be loging in twice
-    if (user && is2FAVerified && (useMatch("/verify-2fa") || useMatch("/login"))) {
+    if (user && is2FAVerified && (isOnLogin || isOnVerify2FA)) {
         return <Navigate to="/dashboard/profile" />;
     }
 
